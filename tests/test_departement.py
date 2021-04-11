ba3e62084add7a72d81e81656dcbb22e899a960e
Pablo Martin-Gomez <pablomg@eskapa.be>
2021-04-11 16:29:33 +0200

    test_departement.py: implement tests for all departements's functions

--- tests/test_departement.py
+++ tests/test_departement.py
@@ -1,5 +1,21 @@
 import pytest
-from scraper.departements import to_departement_number
+from scraper.departements import (
+    to_departement_number,
+    import_departements,
+    cp_to_insee,
+    get_city,
+)
+
+
+def test_import_departements():
+    departements = import_departements()
+
+    assert len(departements) == 101
+    assert departements[:3] == ["01", "02", "03"]
+    assert departements[83] == "83"
+    assert departements[-1] == "976"
+    assert departements.index("2A") == 28
+    assert sorted(departements) == departements
 
 
 def test_insee_to_departement_code():
@@ -22,3 +38,65 @@ def test_insee_to_departement_code():
         to_departement_number(wrong_insee_code)
     with pytest.raises(ValueError):
         to_departement_number(not_in_insee_code_table_insee_code)
+
+
+def test_get_city():
+    address_1 = "2 avenue de la République, 75005 PARIS"
+    address_2 = " 24 Rue de la Brèche, 91740 Pussay "
+    address_3 = "Centre Cial du Bois des Roches 91240 SAINT MICHEL SUR ORGE"
+    address_4 = " , 83700 Saint-Raphaël "
+    address_5 = "1171 Avenue Gaston Feuillard\n97100 Basse-Terre"
+    address_6 = "Rue de la République"
+
+    assert get_city(address_1) == "PARIS"
+    assert get_city(address_2) == "Pussay"
+    assert get_city(address_3) == "SAINT MICHEL SUR ORGE"
+    assert get_city(address_4) == "Saint-Raphaël"
+    assert get_city(address_5) == "Basse-Terre"
+    with pytest.raises(AttributeError):
+        get_city(address_6)
+
+
+def test_cp_to_insee():
+    # Paris 15
+    cp_paris_15 = "75015"
+    insee_paris_15 = "75115"
+    assert cp_to_insee(cp_paris_15) == insee_paris_15
+
+    # Ajaccio
+    cp_ajaccio_1 = "20090"
+    cp_ajaccio_2 = "20090"
+    cp_ajaccio_3 = "20167"
+    insee_ajaccio = "2A004"
+    assert cp_to_insee(cp_ajaccio_1) == insee_ajaccio
+    assert cp_to_insee(cp_ajaccio_2) == insee_ajaccio
+    # assert cp_to_insee(cp_ajaccio_3) == insee_ajaccio ==> faux, renvoie 2A351 (VILLANOVA)
+
+    # Saint-Pierre-Laval
+    cp_saint_pierre_laval = "42620"
+    insee_saint_pierre_laval = "03250"
+    # assert cp_to_insee(cp_saint_pierre_laval) == insee_saint_pierre_laval ==> faux, renvoie 42257 (ST MARTIN D ESTREAUX)
+
+    # Paray-Vieille-Poste
+    cp_paray_vieille_poste = "94390"
+    insee_paray_vieille_poste = "91479"
+    assert cp_to_insee(cp_paray_vieille_poste) == insee_paray_vieille_poste
+
+    # Fort de France
+    cp_fort_de_france = "97234"
+    insee_fort_de_france = "97209"
+    assert cp_to_insee(cp_fort_de_france) == insee_fort_de_france
+
+    # Monaco
+    cp_monaco = "98000"
+    insee_monaco = "99138"
+    assert cp_to_insee(cp_monaco) == insee_monaco
+
+    # CP invalide
+    invalid_cp = "1234"
+    assert cp_to_insee(invalid_cp) == invalid_cp
+
+    # Cholet entier
+    cp_cholet_int = 49300  # => invalide
+    insee_cholet = "49099"
+    assert cp_to_insee(cp_cholet_int) == cp_cholet_int
